64551891693f49153bc3b14805d460053930008c
dkvaja <dushyant@logisticinfotech.co.in>
2022-01-29 14:52:54 +0530

    added favicon

--- live.js
+++ live.js
@@ -1,7 +1,20 @@
-function getvalue() {
+function getvalue(event) {
+  event?.preventDefault();
   var bhav = document.getElementById("priceofsopari");
   var cprice = document.getElementById("pricebycustomer");
-  document.getElementById("gram").innerHTML =
-    Math.round((1000 * cprice.value) / bhav.value);
-}
 
+  let totalGrams = Math.round((1000 * cprice.value) / bhav.value);
+  let totalKilo = 0;
+
+  if (totalGrams >= 1000) {
+    let temp = Math.round(totalGrams / 1000);
+    let tempGram = totalGrams % 1000;
+    totalKilo = temp;
+    totalGrams = tempGram;
+  }
+
+  console.log("totalGrams", totalGrams);
+
+  document.getElementById("kilo").innerHTML = totalKilo;
+  document.getElementById("gram").innerHTML = totalGrams || 0;
+}
